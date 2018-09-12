61fc3d013968987ea558967c723e9d139d03c5c7
VitaliBrych333 <36643236+VitaliBrych333@users.noreply.github.com>
2018-09-12 16:53:51 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,33 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    var sum = currency;
+    const sum = currency;
     if (sum <= 0) return {};
     if (sum > 10000) return {error: "You are rich, my friend! We don't have so much coins for exchange"};
-    var a = Math.floor(sum/50);
-    var b = Math.floor((sum - 50*a)/25);
-    var c = Math.floor((sum - 50*a - 25*b)/10);
-    var d = Math.floor((sum - 50*a -25*b - 10*c)/5);
-    var e = sum - 50*a - 25*b - 10*c - 5*d;
-    var obj = {
-    	"H": a, 
-    	"Q": b, 
-    	"D": c,
-    	"N": d,
-    	"P": e
+    
+    const COUNT_50 = Math.floor(sum / 50);
+          balance_50 = sum % 50;
+          COUNT_25 = Math.floor(balance_50 / 25);
+          balance_25 = balance_50 % 25;
+          COUNT_10 = Math.floor(balance_25 / 10);
+          balance_10 = balance_25 % 10;
+          COUNT_5 = Math.floor(balance_10 / 5);
+          COUNT_1 = balance_10 % 5;
+    
+    let obj = {
+    	"H": COUNT_50, 
+    	"Q": COUNT_25, 
+    	"D": COUNT_10,
+    	"N": COUNT_5,
+    	"P": COUNT_1
     };
     for (key in obj) {
-    	if ( obj[key]==0) delete obj[key];
+      if ( obj[key] == 0) {
+        obj[key] = undefined;
+        obj = JSON.parse(JSON.stringify(obj));
+      }
     }
     return obj;
     // Your code goes here!
     // Return an object containing the minimum number of coins needed to make change
 }
+
